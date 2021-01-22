a1f7c215b051836018192428b64856d0eecd953c
Sizan-Aminun <71691929+sizanaminun@users.noreply.github.com>
2021-01-22 23:24:31 +0600

    Final

--- assignment.js
+++ assignment.js
@@ -1,50 +1,62 @@
 //  1 no part (kilometerToMeter)
 
-function kilometerToMeter(kilometer) {
-    var meter = kilometer * 1000;
-
+function kilometerToMeter(Distance) {
+    var meter = Distance * 1000;//To convert kilometer to meter.
+    if (Distance <= 0) {
+        meter = "Distance Never Negative";
+    }
     return meter;
 }
-var result = kilometerToMeter(4);
+var result = kilometerToMeter(9); //input any number and get the result in meter
 console.log(result);
 
 
+
+
 // 2 no part(budgetCalculator)
+
 function budgetCalculator(watch, phone, laptop) {
-    var result = watch + phone + laptop;
+    var result = (watch * 50) + (phone * 100) + (laptop * 500);
     return result;
 }
-var sum = budgetCalculator(50, 100, 500);
-console.log(sum);
+var total = budgetCalculator(1, 1, 1);//get the total amount of your budget
+console.log(total);
+
 
 
 
 //  3 no part(hotelCost)
 
-function hotelCost() {
-    var days = 12;
+function hotelCost(days) {
+    var days = 50;
     var hotelCost = 0;
     if (days <= 10) {
-        hotelCost = days * 100;
+        hotelCost = days * 100;// Renting cost of 1st ten days
     }
+
     else if (days <= 20) {
         var firstTenDays = 10 * 100;
         var remainingDays = days - 10;
         var secondTenDays = remainingDays * 80;
-        hotelCost = firstTenDays + secondTenDays;
+        hotelCost = firstTenDays + secondTenDays;//Renting cost of 2nd ten days
     }
     else {
         var firstTenDays = 10 * 100;
         var secondTenDays = 10 * 80;
         var remainingDays = days - 20;
         var thirdTenDays = remainingDays * 50;
-        hotelCost = firstTenDays + secondTenDays + thirdTenDays;
+        hotelCost = firstTenDays + secondTenDays + thirdTenDays;//Renting cost of 3rd ten days
     }
     if (days <= 0) {
         hotelCost = "Don't want to stay here? ";
     }
     return hotelCost;
 }
+var total = hotelCost(12);
+console.log(total);
+
+
+
 
 
 // 4 no part(megaFriend)
@@ -56,11 +68,7 @@ function megaFriend(largestWord) {
             largest = element;
         }
     }
-    return largest;
+    return largest;//Get the largest name
 }
-var bigName = megaFriend(["Tweety", "Hedwig", "Peafowl", "Columbidae", "Zippy", "Scruffy"]);
-console.log(bigName);
-
-
-
-
+var nameSet = megaFriend(["Tweety", "Hedwig", "Peafowl", "Columbidae", "Zippy", "Scruffy"]);
+console.log(nameSet)
